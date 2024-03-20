81215810c3f514e4e58d2de9c3616e265a963f19
PhilippeRobichaudGionet <prg@hotmail.ca>
2024-03-20 17:49:12 -0400

    sa

---
+++ Wet.py
@@ -0,0 +1,27 @@
+import grovepi
+
+class Wet:
+
+    wet = 0
+    wetTarget = None
+
+    def __init__(self):
+        [temp,humid] = grovepi.dht(2,0)
+        self.wet = humid
+    #GET Wet
+    def GetWet(self):
+        return self.wet + "%"
+    #SET Wet
+    def SetWet(self,newValue):
+        self.wet = newValue
+   
+    #GET WetTarget
+    def GetWetTarget(self):
+        return self.wetTarget
+    #SET WetTarget
+    def SetWetTarget(self,NewValue):
+        if (not(NewValue < 0 or NewValue > 100)):
+            self.wetTarget = NewValue
+    #Compare Humidificator desactivation
+    def CompareHumidtity(self):
+        self.wetTarget - self.wet == 5
--- temp.py
+++ temp.py
@@ -1,9 +1,15 @@
 import grovepi
+import time
 
 class Temp:
 
     temp = 0
     tempCible = None
+    
+    def __init__(self):
+        [temp,humidity] = grovepi.dht(2,0)
+        self.temp = temp
+
     #GET temp
     def GetTemp(self):
         return self.temp
@@ -19,6 +25,13 @@ class Temp:
         if (not(newValue < 0 or newValue > 30)):
             self.tempCible = newValue
     
+
+    #Compare Activation Chauffage
+    def CompareTemp(self): 
+        ValStart = self.tempCible - self.temp == 2
+        time.sleep(10)
+        ValEnd = self.tempCible - self.temp == 2
+        return ValStart == ValEnd
 
 
 
