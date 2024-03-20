da3b03a5d8feee4c98a744a23c24fcfcf19ad3c3
MathieuLeroux1 <116597068+MathieuLeroux1@users.noreply.github.com>
2024-03-20 17:43:43 -0400

    debut_capteurs

--- Maison.py
+++ Maison.py
@@ -8,6 +8,9 @@ import Actions
 class Maison:
     def __init__(self):
         self.lock = threading.Lock()
+        self.courtier = "test.mosquitto.org"
+        self.port_courtier = 1883
+        self.sujet = "clg/kf1/maison/temp"
 
     def demarrer(self):
         pass
