b5b8feff93618fe1b60f0cc3b385529693a6fb56
PhilippeRobichaudGionet <prg@hotmail.ca>
2024-03-25 17:34:54 -0400

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -1,15 +1,18 @@
 import grovepi
 import time
 import temp
-
+import Wet
+import threading
+import Capteurs
 class InterfaceUtilisateur:
-    mode = ["Local", "Distant", "Quitter"]
-
+    modes = ["Local", "Distant", "Quitter"]
     def __init__(self, lock, bouton_pin):
         with self.lock:
             self.lock = lock
             self.bouton_pin = bouton_pin
             grovepi.pinMode(self.bouton_pin, "INPUT")
+            self.thread = threading.Thread(target=self.changer_interface())
+            self.thread.start()
           
     def bouton_appuye(self):
         with self.lock:
@@ -20,27 +23,40 @@ class InterfaceUtilisateur:
                 time.sleep(0.1)
 
     def changer_interface(self):
-        temperature_cible = None
-        humidite_cible = None
+        temperature_cible = 0 
+        humidite_cible = 0
         mode_selectionne = None
+        RotaryPin = 1
+        tempe = temp()
+        grovepi.pinMode(RotaryPin,"INPUT")
 
+        #Waiting Room
         while not self.bouton_appuye():
             pass
 
-        # Modification de la température
-        
+        # Modification de la température        
         while not self.bouton_appuye():
-            pass
+            sensor_value = grovepi.analogRead(RotaryPin)
+            degree = map(sensor_value,0,1023,0,30)
+            tempe.SetTempCible(degree)
+            temperature_cible = degree
 
-        # Modification de l'humidité
-        
+        # Modification de l'humidité           
         while not self.bouton_appuye():
-            pass
+            sensor_value = grovepi.analogRead(RotaryPin)
+            degree = map(sensor_value,0,1023,0,100)
+            tempe.SetWetTarget(degree)
+            humidite_cible = degree
 
-        # Sélection du mode
-        
+        # Sélection du mode     
         while not self.bouton_appuye():
-            pass
+            sensor_value = grovepi.analogRead(RotaryPin)
+            degree = map(sensor_value,0,1023,0,2)
+            mode_selectionne = self.modes[degree]
+
+
+            
+
 
         return temperature_cible, humidite_cible, mode_selectionne
 
