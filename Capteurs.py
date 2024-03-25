3434e83f1951e600cac2d108a80d084b89df518c
MathieuLeroux1 <116597068+MathieuLeroux1@users.noreply.github.com>
2024-03-25 17:15:00 -0400

    Update Capteurs.py

--- Capteurs.py
+++ Capteurs.py
@@ -2,6 +2,8 @@ import paho.mqtt.client as mqtt
 import grovepi
 import Temp
 import Wet
+from grove_rgb_lcd import *
+from time import sleep
 
 class Capteurs:
     def __init__(self, lock, courtier, port_courtier, sujet, mode="Local"):
@@ -15,23 +17,41 @@ class Capteurs:
 
         self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "Equipe1")
         self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
         self.client.connect(self.courtier, self.port_courtier)
 
     def on_connect(self, client, userdata, flags, rc):
         client.subscribe(self.sujet)
     
+    def on_message(self,client,userdata,message):
+        with self.lock:
+            global temperature_equipier
+            temperature_equipier = message.payload.decode()
+            setText("Distant: Température: °C")
+
     def lire_capteurs(self):
         with self.lock:
             temperature = self.temp_obj.GetTemp()
             humidite = self.wet_obj.GetWet()
             return temperature, humidite
 
+    def lireValeursDistantes(self, client):
+        client.loop_start()
+        while(True):
+            with self.lock:
+                if(temperature_equipier == None):
+                    sleep(1)
+                else:
+                    break
+        client.loop_stop()
+        
         
     def mettre_a_jour_lcd(self, temperature, humidite):
-        if(self.mode == "Local"):
-            pass
-        elif(self.mode == "Distant"):
-            pass
+        if self.mode == "Local":
+            texte = "Local: Température: {}°C, Humidité: {}%".format(temperature, humidite)
+            setText(texte)
+        elif self.mode == "Distant":
+            self.lireValeursDistantes(self.client)
         else:
             self.client.disconnect
         
