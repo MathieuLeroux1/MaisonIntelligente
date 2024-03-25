0f48481f236872d7698641a162831c3b085eb885
MathieuLeroux1 <116597068+MathieuLeroux1@users.noreply.github.com>
2024-03-25 15:49:11 -0400

    Update Capteurs.py

--- Capteurs.py
+++ Capteurs.py
@@ -1,17 +1,32 @@
+import paho.mqtt.client as mqtt
+import grovepi
+import math
+import time
+
 class Capteurs:
-    def __init__(self, lock, mode = "Local"):
+
+
+    def __init__(self, lock, courtier, port_courtier, sujet, mode="Local"):
         self.lock = lock
         self.mode = mode
-    
+        self.courtier = courtier
+        self.port_courtier = port_courtier
+        self.sujet = sujet
+
+        self.client = mqtt.Client("Capteurs_Client")
+        self.client.connect(self.courtier, self.port_courtier)
+
     def lire_capteurs(self):
-        pass
+        with self.lock:
+            temperature, humidite = grovepi.dht(7, 0) 
+            return temperature, humidite
         
     def mettre_a_jour_lcd(self, temperature, humidite):
         pass
         
-    def publier_informations(self, temperature, humidite, mode):
-        pass
+    def publier_informations(self, temperature, humidite):
+        payload = "Température: {}°C, Humidité: {}%".format(temperature, humidite)
+        self.client.publish(self.sujet, payload)
 
     def set_mode(self, mode):
-        self.mode = mode
-    
+        self.mode = mode        
