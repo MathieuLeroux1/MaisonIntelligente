5eca5eedcbadfb5e6e384b113aa4c8654f74c354
PhilippeRobichaudGionet <prg@hotmail.ca>
2024-03-27 17:23:54 -0400

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -7,6 +7,7 @@ from Capteurs import Capteurs
 
 class Interface:
     modes = ["Local", "Distant", "Quitter"]
+
     def __init__(self, lock, bouton_pin, capteurs):
         self.lock = lock
         self.capteurs = capteurs
@@ -26,8 +27,7 @@ class Interface:
             #Création du thread et exécution du thread Interface
             self.thread = threading.Thread(target=self.changer_interface())
             self.thread.start()
-
-          
+         
     def bouton_appuye(self):
         with self.lock:
             while True:
@@ -37,45 +37,22 @@ class Interface:
                 time.sleep(0.1)
 
     def changer_interface(self):
-<<<<<<< HEAD
  
-=======
-        mode_selectionne = None
-        RotaryPin = 1
-        tempe = Temp()
-        Wets = Wet()
-        grovepi.pinMode(RotaryPin,"INPUT")
-
-        #Waiting Room
-        while not self.bouton_appuye():
-            pass
-        
->>>>>>> bf5dcec986148d492a3b257e26d85850c77a6a78
         # Modification de la température cible
         while not self.bouton_appuye():
             with self.lock:
                 sensor_value = grovepi.analogRead(self.RotaryPin)
                 degree = map(sensor_value,0,1023,0,30)
-<<<<<<< HEAD
                 self.tempe.SetTempCible(degree)
                 self.capteurs.mettre_a_jour_lcd("Température cible: {}°C".format(self.tempe.GetTempCible()))
-=======
-                tempe.SetTempCible(degree)
-                self.capteurs.mettre_a_jour_lcd("Température cible: {}°C".format(tempe.GetTempCible()))
->>>>>>> bf5dcec986148d492a3b257e26d85850c77a6a78
 
         # Modification de l'humidité cible
         while not self.bouton_appuye():
             with self.lock:
                 sensor_value = grovepi.analogRead(self.RotaryPin)
                 degree = map(sensor_value,0,1023,0,100)
-<<<<<<< HEAD
                 self.Wets.SetWetTarget(degree)
                 self.capteurs.mettre_a_jour_lcd("Humidité cible: {}°C".format(self.Wets.GetWetTarget()))
-=======
-                Wets.SetWetTarget(degree)
-                self.capteurs.mettre_a_jour_lcd("Humidité cible: {}°C".format(Wets.GetWetTarget()))
->>>>>>> bf5dcec986148d492a3b257e26d85850c77a6a78
 
         # Sélection du mode
         while not self.bouton_appuye():
