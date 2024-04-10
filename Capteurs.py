ce4c188326f46e15262c263eaf8e83c14321682f
MathieuLeroux1 <116597068+MathieuLeroux1@users.noreply.github.com>
2024-04-10 17:23:25 -0400

    Update Capteurs.py

--- Capteurs.py
+++ Capteurs.py
@@ -20,7 +20,7 @@ class Capteurs:
         self.temp_initale = Temp()
         self.wet_initiale = Wet()
         
-        self.mettre_a_jour_lcd("Température: " + self.temp_initale + "; Humidité: " + self.wet_initiale)
+        self.mettre_a_jour_lcd("Température: " + self.temp_initale.GetTemp() + "; Humidité: " + self.wet_initiale.GetWet())
         
         self.mode = mode
         self.courtier = courtier
