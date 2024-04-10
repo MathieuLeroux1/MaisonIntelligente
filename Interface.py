fde717795185bbb9a3c48d2efba73606538433a8
PhilippeRobichaudGionet <prg@hotmail.ca>
2024-04-10 17:03:36 -0400

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -51,14 +51,13 @@ class Interface:
             time.sleep(0.5)
 
             if (zone == "ModifTemp"):
-                print("salut")
                 sensor_value = grovepi.analogRead(self.RotaryPin)
-                degree = map(sensor_value, 0, 1023, 0, 30)
                 try:
-                    self.tempe.SetTempCible(degree)
+                    degree = map(sensor_value, 0, 1023, 0, 30)
                 except Exception as e:
-                    self.capteurs.mettre_a_jour_lcd("Température cible: {}°C".format(self.tempe.GetTempCible()))
-                    print("bug LCD")
+                    print("bug")
+                self.tempe.SetTempCible(degree)
+                self.capteurs.mettre_a_jour_lcd("Température cible: {}°C".format(self.tempe.GetTempCible()))
             elif (zone == "ModifHumid"):
                 sensor_value = grovepi.analogRead(self.RotaryPin)
                 degree = map(sensor_value, 0, 1023, 0, 100)
