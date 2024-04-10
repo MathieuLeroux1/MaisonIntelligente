5cf153495a1818a87ed263e164b1c65dd38a0669
PhilippeRobichaudGionet <prg@hotmail.ca>
2024-04-10 15:27:54 -0400

    Update Capteurs.py

--- Capteurs.py
+++ Capteurs.py
@@ -16,13 +16,11 @@ class Capteurs:
         self.sujet = sujet
         self.temperature_equipier = None
 
-        try:
-            self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "Equipe1")
-            self.client.on_connect = self.on_connect
-            self.client.on_message = self.on_message
-            self.client.connect(self.courtier, self.port_courtier)
-        except Exception as e:
-            print(e)
+
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "Equipe1")
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.connect(self.courtier, self.port_courtier)
 
     def on_connect(self, client, userdata, flags, rc):
         client.subscribe(self.sujet)
