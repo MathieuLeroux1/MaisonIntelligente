7dcaba91491ba87e23ea1f76ffb40e508fa6feae
MathieuLeroux1 <116597068+MathieuLeroux1@users.noreply.github.com>
2024-04-10 16:02:51 -0400

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -26,11 +26,15 @@ class Interface:
          
     def bouton_appuye(self):
         with self.lock:
-            while True:
+            print("Entering bouton_appuye()")
+            bouton_presse = False
+            while not bouton_presse:
                 etat_bouton = grovepi.digitalRead(self.bouton_pin)
                 if etat_bouton == 1:
-                    return True
-                time.sleep(0.1)
+                    bouton_presse = True
+            print("Exiting bouton_appuye()")
+            return True
+
 
     def changer_interface(self):
         # Attendre l'appui sur le bouton
