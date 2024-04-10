f7a43c5e8d88a09c521c30425446534264522b86
PhilippeRobichaudGionet <prg@hotmail.ca>
2024-04-10 16:49:24 -0400

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -60,7 +60,6 @@ class Interface:
 
 
     def changer_interface(self):
-        print(not(self.bouton_appuye()))
         # Attendre l'appui sur le bouton
         while not self.bouton_appuye():
             pass
